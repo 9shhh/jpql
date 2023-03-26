a107bcfb297b7b7c68a43700e07d5b491b9c2af9
9shhh <uelj1234@naver.com>
2023-03-26 22:21:30 +0900

    페이징 실습

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,9 +1,11 @@
 package hellojpa;
 
 import hellojpa.jpql.Member;
-import hellojpa.jpql.MemberDTO;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class Main {
@@ -15,33 +17,22 @@ public class Main {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
-            List<Member> result = em.createQuery("select m from Member m", Member.class) // select 절에 정의된 엔티티 모두 영속성 컨텍스트에서 관리됨.
-                    .getResultList();
-            Member findMember = result.get(0);
-            findMember.setAge(20); // 따라서 해당 코드처럼 속성 값을 변경하면 정상적으로 데이터 반영됨.
-
-            // 여러 값 조회
-            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
                     .getResultList();
 
-            Object[] resultGetFirst = resultList.get(0);
-            System.out.println("username = " + resultGetFirst[0]);
-            System.out.println("age = " + resultGetFirst[1]);
-
-            // 여러 값 조회 - new 명령어로 조회
-            // 단순 값을 DTO 로 바로 조회
-            // MemberDTO 패키지 명을 다 적어줘야함. -> JPQL 이 문자열이라서...
-            List<MemberDTO> resultList1 = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            MemberDTO memberDTO = resultList1.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+            System.out.println("resultList.size() = " + resultList.size());
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
