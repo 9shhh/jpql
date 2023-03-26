b24efe186295c3f8ca2a0c1f7f59ce5e67925300
9shhh <uelj1234@naver.com>
2023-03-26 23:09:22 +0900

    조인 실습

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import hellojpa.jpql.Member;
+import hellojpa.jpql.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,23 +17,45 @@ public class Main {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(20);
+            member.changeTeam(team);
 
-            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
+            em.persist(member);
+
+            String query = "select m from Member m join m.team t";
+            em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("resultList.size() = " + resultList.size());
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
+            // 세타 조인(cross join)
+            String query2 = "select m from Member m, Team t where m.username = t.name";
+            em.createQuery(query2, Member.class)
+                    .getResultList();
+
+            // 조인 대상 필터링
+            // 회원과 팀을 조인, 팀 이름이 teamA 인 팀만 조인
+            // ...
+            // and ( -> 이런식으로 조인 조건이 들어감
+            //         team1_.name='teamA'
+            // )
+            String query3 = "select m from Member m left join m.team t on t.name = 'teamA'";
+            em.createQuery(query3, Member.class)
+                    .getResultList();
+
+            // 연관관계 없는 엔티티 외부 조인
+            // 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+            // Team team1_
+            //      on (
+            //         member0_.username=team1_.name
+            //      )
+            String query4 = "select m from Member m join Team t on m.username = t.name";
+            em.createQuery(query4, Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
